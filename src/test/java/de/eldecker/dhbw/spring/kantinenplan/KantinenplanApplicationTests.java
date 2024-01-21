6242037541610e90ecdead79a9ece67fd618f8e9
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-01-21 13:01:43 +0100

    Projektvorlage von start.spring.io: https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.2.2&packaging=jar&jvmVersion=17&groupId=de.eldecker.dhbw.spring&artifactId=kantinenplan&name=Kantinenplan&description=REST-API%20f%C3%BCr%20Kantinenplan&packageName=de.eldecker.dhbw.spring.kantinenplan&dependencies=web

---
+++ src/test/java/de/eldecker/dhbw/spring/kantinenplan/KantinenplanApplicationTests.java
@@ -0,0 +1,13 @@
+package de.eldecker.dhbw.spring.kantinenplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KantinenplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
