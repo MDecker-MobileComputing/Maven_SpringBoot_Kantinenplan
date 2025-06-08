645c4675a54683d6b2a429508d71d20b566e3b26
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-06-08 10:49:17 +0200

    Kaputte JSON-Datei fuer Talend API Test geloescht, JavaDoc-Kommentare in KantinenException dazu

--- src/main/java/de/eldecker/dhbw/spring/kantinenplan/model/KantinenException.java
+++ src/main/java/de/eldecker/dhbw/spring/kantinenplan/model/KantinenException.java
@@ -1,9 +1,18 @@
 package de.eldecker.dhbw.spring.kantinenplan.model;
 
 
+/**
+ * Eigene Exception-Klasse.
+ */
 @SuppressWarnings("serial")
 public class KantinenException extends Exception {
 
+    
+    /**
+     * Konstruktor der Klasse.
+     * 
+     * @param fehlerbeschreibung Beschreibung des Fehlers
+     */
     public KantinenException( String fehlerbeschreibung ) {
         
         super( fehlerbeschreibung );
